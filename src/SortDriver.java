903c97d63a00be35d5817e36dfa3db42b6ddc395
ajrod <ajrodz2004@gmail.com>
2023-11-14 13:35:42 -0500

    Initial Commit

--- src/SortDriver.java
+++ src/SortDriver.java
@@ -1,2 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SortDriver {
+    public static void main(String[] args) {
+        try {
+            File file = new File(args[0]);
+            Scanner fileInput = new Scanner(file);
+            int[] array = new int[10000];
+            int num = 0;
+            while (fileInput.hasNextInt()) {
+                array[num] = fileInput.nextInt();
+                num++;
+            }
+            Scanner input = new Scanner(System.in);
+            System.out.println("selection-sort (s) merge-sort (m) heap-sort (h) quick-sort-fp (q) quick-sort-rp (r)");
+            System.out.print("Enter the algorithm: ");
+            String alg = input.next();
+            switch (alg.toLowerCase()) {
+                case "s":
+                    Sorting.SelectionSort(array);
+                    break;
+                case "m":
+                    Sorting.MergeSort(array);
+                    break;
+                case "h":
+                    Sorting.HeapSort(array);
+                    break;
+                case "q":
+                    Sorting.QuickSortFirst(array);
+                    break;
+                case "r":
+                    Sorting.QuickSortRandom(array);
+                    break;
+            }
+        } catch (FileNotFoundException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Input File Not Provided");
+        }
+    }
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,2 +1,17 @@
 public class Sorting {
+    public static int SelectionSort(int[] input) {
+        throw new RuntimeException("NYI");
+    }
+    public static int MergeSort(int[] input) {
+        throw new RuntimeException("NYI");
+    }
+    public static int HeapSort(int[] input) {
+        throw new RuntimeException("NYI");
+    }
+    public static int QuickSortFirst(int[] input) {
+        throw new RuntimeException("NYI");
+    }
+    public static int QuickSortRandom(int[] input) {
+        throw new RuntimeException("NYI");
+    }
 }
